99fd0d6c75f4fa02cad011986e73be40f2800b3d
BreyneWard <wardbreyne@hotmail.com>
2022-12-21 17:17:56 +0100

    9.5. bibliotheek

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _9._5._bibliotheek;
+class Program
+{
+    static void Main(string[] args)
+    {
+        BibBoek boek1 = new BibBoek();
+        boek1.NaamOntlener = Input.AskString("Geef je naam op: ");
+        Console.WriteLine($"Naam ontlener: {boek1.NaamOntlener}, Uitleendatum {boek1.Vandaag}");
+        Console.WriteLine($"Tijdstip inlevering: {boek1.Inleverdag}");
+
+    }
+
+    // Classes
+    public class BibBoek
+    {
+        public string NaamOntlener { get; set; }
+        public DateTime Vandaag { get; private set; } = DateTime.Now;
+        public DateTime Inleverdag { get; } = DateTime.Now.AddDays(14);
+
+    }
+
+    public static class Input
+    {
+        public static string AskString(string question)
+        {
+            Console.Write($"{question}");
+            return Console.ReadLine() ?? string.Empty;
+        }
+    }
+}
